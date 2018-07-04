cb397a11b242885d2f9635c2347f1a80c16d1825
Mikhail Golubev <mgolubev86@gmail.com>
2018-07-04 14:47:07 +0000

    some setparams.py tests and first argument must be the id

--- setparams.py
+++ setparams.py
@@ -1,7 +1,7 @@
 import fcntl
 import sys
 
-ID = 127
+ID = sys.argv[1] 
 
 IOCTL_CMD_REG_FREQUENCY, IOCTL_CMD_CLEAR_AND_START_COUNTER, IOCTL_CMD_LEAD_COUNTER_AND_TOF, IOCTL_CMD_INVERSE_SIGNAL, IOCTL_CMD_FORBID_ALLOW_CLEAR_COUNTER = range(5)
 
@@ -35,7 +35,34 @@ f0.close()
 
 f0 = open('/dev/pnpipcinc{}counter0'.format(ID), 'r')
 f1 = open('/dev/pnpipcinc{}counter1'.format(ID), 'r')
-print(f0.readline())
-print(f1.readline())
+print("12500 Hz 1 sec ", f0.readline())
+print("12500 Hz 1 sec ", f1.readline())
+f1.close()
+f0.close()
+
+f0 = open('/dev/pnpipcinc{}counter0'.format(ID), 'wb')
+f1 = open('/dev/pnpipcinc{}counter1'.format(ID), 'wb')
+fcntl.ioctl(f0, IOCTL_CMD_REG_FREQUENCY, FREQUENCY_100KHZ+32)
+f0.write('1000000000')
+
+fcntl.ioctl(f1, IOCTL_CMD_REG_FREQUENCY, FREQUENCY_100KHZ+32)
+f1.write('100000000000')
+
+fcntl.ioctl(f0, IOCTL_CMD_CLEAR_AND_START_COUNTER, 12)
+fcntl.ioctl(f1, IOCTL_CMD_CLEAR_AND_START_COUNTER, 12)
+fcntl.ioctl(f0, IOCTL_CMD_CLEAR_AND_START_COUNTER, 13)
+fcntl.ioctl(f1, IOCTL_CMD_CLEAR_AND_START_COUNTER, 13)
+fcntl.ioctl(f0, IOCTL_CMD_CLEAR_AND_START_COUNTER, 15)
+fcntl.ioctl(f1, IOCTL_CMD_CLEAR_AND_START_COUNTER, 15)
+
+time.sleep(1)
+
+f1.close()
+f0.close()
+
+f0 = open('/dev/pnpipcinc{}counter0'.format(ID), 'r')
+f1 = open('/dev/pnpipcinc{}counter1'.format(ID), 'r')
+print("100 KHz 1 sec ", f0.readline())
+print("100 KHz 1 sec ", f1.readline())
 f1.close()
 f0.close()
