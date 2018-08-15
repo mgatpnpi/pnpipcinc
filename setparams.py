e862ee17289248af068195195fb0193406881584
Mikhail Golubev <mgolubev86@gmail.com>
2018-08-15 08:53:05 +0000

    test python script modified

--- setparams.py
+++ setparams.py
@@ -16,20 +16,16 @@ IOCTL_CMD_ALLOW_COUNTERS = ord('x') << 8 | 4
 
 FREQUENCY_1MHZ, FREQUENCY_500KHZ, FREQUENCY_250KHZ, FREQUENCY_125KHZ, FREQUENCY_100KHZ, FREQUENCY_50KHZ, FREQUENCY_25KHZ, FREQUENCY_12500HZ = range(8)
 
-f0 = os.open('/dev/pnpipcinc{}counter0'.format(ID), os.O_RDWR)
-f1 = os.open('/dev/pnpipcinc{}counter1'.format(ID), os.O_RDWR)
+f0 = os.open('/dev/pnpipcinc{}timer0'.format(ID), os.O_RDWR)
+f1 = os.open('/dev/pnpipcinc{}timer1'.format(ID), os.O_RDWR)
 
-fcntl.ioctl(f0, IOCTL_CMD_LEAD_COUNTER_AND_TOF, 0)
-fcntl.ioctl(f0, IOCTL_CMD_INVERSE_SIGNAL_COUNTERS, 0)
-fcntl.ioctl(f0, IOCTL_CMD_ALLOW_COUNTERS, 0)
-fcntl.ioctl(f0, IOCTL_CMD_REG_FREQUENCY, FREQUENCY_12500HZ+32) // 32 (1<<5) set for the counter that stops another on preset reached
+fcntl.ioctl(f0, IOCTL_CMD_REG_FREQUENCY, FREQUENCY_12500HZ+32) // 32 (1<<5) # set for the counter that stops another on preset reached
 fcntl.ioctl(f1, IOCTL_CMD_REG_FREQUENCY, FREQUENCY_250KHZ)
 fcntl.ioctl(f0, IOCTL_CMD_CLEAR_AND_START_COUNTER, 4)
 fcntl.ioctl(f1, IOCTL_CMD_CLEAR_AND_START_COUNTER, 4)
 os.pwrite(f0, b'20000', 0)
 os.pwrite(f1, b'900000', 0)
 
-fcntl.ioctl(f0, IOCTL_CMD_ALLOW_COUNTERS, 17)
 fcntl.ioctl(f0, IOCTL_CMD_CLEAR_AND_START_COUNTER, 20)
 fcntl.ioctl(f1, IOCTL_CMD_CLEAR_AND_START_COUNTER, 20)
 fcntl.ioctl(f0, IOCTL_CMD_CLEAR_AND_START_COUNTER, 21)
